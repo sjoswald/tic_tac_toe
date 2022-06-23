29f8a27c7e3bb445b5a98f2df4e313cd9f672482
Stacy Oswald <stacy.oswald@stacys-mbp.home>
2022-06-23 16:49:59 +0100

    X player can play, and not in played cells

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,24 +2,35 @@ require_relative '../tic_tac_toe.rb'
 
 describe 'grid' do
     it 'is an array of length 9' do
-        board=Tictacboard.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        expect(board.grid.size).to eq(9)
+        board=Tictacboard.new()
+        expect(board.grid.size).to eq(3)
         
     end
 end
 
 describe 'cell_checker' do
     it 'can retrieve contents of a cell' do
-        board=Tictacboard.new([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
+        board=Tictacboard.new()
         expect(board.cell_checker(1,1)).to eq(5)
     end
 end
 
 describe 'x_player_mark' do
     it 'can put an X in a cell' do
-        board=Tictacboard.new([[1, 2, 3],[4, 5, 6],[7, 8, 9]])
+        board=Tictacboard.new()
         board.x_player_mark(0,1)
         expect(board.grid[0][1]).to eq("X")
         puts board.grid
     end
-end
+
+    it 'can only put an X in an unplayed cell' do
+        board=Tictacboard.new([["O", 2, 3],[4, 5, 6],[7, 8, 9]])
+        board.x_player_mark(0,0)
+        expect(board.grid[0][0]).to eq("O")
+    end
+
+    it 'shouts when you play a played cell' do
+        board=Tictacboard.new([["O", 2, 3],[4, 5, 6],[7, 8, 9]])
+        expect{board.x_player_mark(0,0)}.to output("Don't be silly! This square is taken.").to_stdout
+    end
+end 
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,8 @@
 class Tictacboard
   attr_accessor :grid
 
-  def initialize (grid)
-      @grid = grid        
+  def initialize (grid = [[1, 2, 3],[4, 5, 6],[7, 8, 9]] )
+      @grid = grid
   end
 
   def cell_checker(x,y)
@@ -10,6 +10,10 @@ class Tictacboard
   end
 
   def x_player_mark(x,y)
-      @grid[x][y] = "X"
+      unless @grid[x][y] == "X" || @grid[x][y] == "O"
+        @grid[x][y] = "X" 
+      else
+        print "Don't be silly! This square is taken."
+      end
   end
 end
