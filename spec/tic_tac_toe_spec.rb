5b12de0222665245902fd4bda8986d968f3dd0b6
Stacy Oswald <stacy.oswald@stacys-mbp.home>
2022-06-28 12:18:00 +0100

    Win condition implementation

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,7 +3,7 @@ require_relative '../tic_tac_toe.rb'
 describe 'grid' do
     it 'is an array of length 9' do
         board=Tictacboard.new()
-        expect(board.grid.size).to eq(3)
+        expect(board.grid.size).to eq(9)
         
     end
 end
@@ -11,46 +11,97 @@ end
 describe 'cell_checker' do
     it 'can retrieve contents of a cell' do
         board=Tictacboard.new()
-        expect(board.cell_checker(1,1)).to eq(5)
+        expect(board.cell_checker(5)).to eq(5)
     end
 end
 
 describe 'x_player_mark' do
     it 'can put an X in a cell' do
         board=Tictacboard.new()
-        board.x_player_mark(0,1)
-        expect(board.grid[0][1]).to eq("X")
+        board.x_player_mark(1)
+        expect(board.grid[0]).to eq("X")
         puts board.grid
     end
 
     it 'can only put an X in an unplayed cell' do
-        board=Tictacboard.new([["O", 2, 3],[4, 5, 6],[7, 8, 9]])
-        board.x_player_mark(0,0)
-        expect(board.grid[0][0]).to eq("O")
+        board=Tictacboard.new(["O", 2, 3, 4, 5, 6, 7, 8, 9])
+        board.x_player_mark(1)
+        expect(board.grid[0]).to eq("O")
     end
 
     it 'shouts when you play a played cell' do
-        board=Tictacboard.new([["O", 2, 3],[4, 5, 6],[7, 8, 9]])
-        expect{board.x_player_mark(0,0)}.to output("Don't be silly! This square is taken.").to_stdout
+        board=Tictacboard.new(["O", 2, 3, 4, 5, 6, 7, 8, 9])
+        expect{board.x_player_mark(1)}.to output("Don't be silly! This square is taken.").to_stdout
     end
 end 
 
-describe 'O_player_mark' do
+describe 'o_player_mark' do
     it 'can put an O in a cell' do
         board=Tictacboard.new()
-        board.o_player_mark(0,1)
-        expect(board.grid[0][1]).to eq("O")
+        board.o_player_mark(1)
+        expect(board.grid[0]).to eq("O")
         puts board.grid
     end
 
-    it 'can only put an X in an unplayed cell' do
-        board=Tictacboard.new([["X", 2, 3],[4, 5, 6],[7, 8, 9]])
-        board.o_player_mark(0,0)
-        expect(board.grid[0][0]).to eq("X")
+    it 'can only put an O in an unplayed cell' do
+        board=Tictacboard.new(["X", 2, 3, 4, 5, 6, 7, 8, 9])
+        board.o_player_mark(1)
+        expect(board.grid[0]).to eq("X")
     end
 
     it 'shouts when you play a played cell' do
-        board=Tictacboard.new([["X", 2, 3],[4, 5, 6],[7, 8, 9]])
-        expect{board.o_player_mark(0,0)}.to output("Don't be silly! This square is taken.").to_stdout
+        board=Tictacboard.new(["X", 2, 3, 4, 5, 6, 7, 8, 9])
+        expect{board.o_player_mark(1)}.to output("Don't be silly! This square is taken.").to_stdout
     end
 end 
+
+describe 'turn-tracking' do
+    it 'knows when one turn has been played' do
+        board=Tictacboard.new(["X", 2, 3, 4, 5, 6, 7, 8, 9])
+        expect(turn_count(board)).to eq(1)
+    end
+
+    it 'knows when three turns have been played' do
+        board=Tictacboard.new(["X", 2, "O", 4, 5, 6, "X", 8, 9])
+        expect(turn_count(board)).to eq(3)
+    end
+    
+    it 'knows when the board is full' do
+        board=Tictacboard.new(["X", "X", "O", "O", "O", "X", "X", "X", "O"])
+        expect(turn_count(board)).to eq(9)
+    end
+end
+
+describe 'GAME-OVER' do
+    it 'knows when player X won on the top row' do
+        board=Tictacboard.new(["X", "X", "X", "O", 5, 6, "O", 8, 9])
+        expect{board_state_checker(board)}.to output("Player X wins!").to_stdout
+    end
+
+    it 'knows when player O won on the first column' do
+        board=Tictacboard.new(["O", "X", "X", "O", 5, 6, "O", 8, 9])
+        expect{board_state_checker(board)}.to output("Player O wins!").to_stdout
+    end
+
+    it 'knows when player X won on the diagonal' do
+        board=Tictacboard.new(["X", "O", "X", "O", "X", 6, "O", 8, "X"])
+        expect{board_state_checker(board)}.to output("Player X wins!").to_stdout
+    end
+
+    it 'knows when player O won on the top row' do
+        board=Tictacboard.new(["O", "O", "O", "X", 5, 6, "X", 8, 9])
+        expect{board_state_checker(board)}.to output("Player O wins!").to_stdout
+    end
+end
+
+=begin
+    win conditions:
+        [0][1][2]
+        [4][5][6]
+        [7][8][9]
+        [0][4][7]
+        [1][5][8]
+        [2][6][9]
+        [0][5][9]
+        [2][5][7]
+=end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,27 +1,61 @@
 class Tictacboard
   attr_accessor :grid
 
-  def initialize (grid = [[1, 2, 3],[4, 5, 6],[7, 8, 9]] )
+  def initialize (grid = [1, 2, 3, 4, 5, 6, 7, 8, 9] )
       @grid = grid
   end
 
-  def cell_checker(x,y)
-      @grid[x][y]
+  def cell_checker(x)
+      @grid[x-1]
   end
 
-  def x_player_mark(x,y)
-      unless @grid[x][y] == "X" || @grid[x][y] == "O"
-        @grid[x][y] = "X" 
+  def x_player_mark(x)
+      unless @grid[x-1] == "X" || @grid[x-1] == "O"
+        @grid[x-1] = "X" 
       else
         print "Don't be silly! This square is taken."
       end
   end
 
-  def o_player_mark(x,y)
-    unless @grid[x][y] == "X" || @grid[x][y] == "O"
-      @grid[x][y] = "O"  
+  def o_player_mark(x)
+    unless @grid[x-1] == "X" || @grid[x-1] == "O"
+      @grid[x-1] = "O"  
     else
       print "Don't be silly! This square is taken."
     end  
   end
+end
+
+def turn_count(board)
+  count = 0
+  board.grid.each { |square|
+    if square == 'X' || square == 'O'
+      count += 1
+    end
+  }
+  count
+end
+
+$win_conditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [0, 4, 8],
+  [2, 5, 8],
+  [1, 4, 7],
+  [2, 4, 6]
+]
+
+def board_state_checker(board)
+  $win_conditions.each { |win_combo|
+  
+    pos_1 = board.grid[win_combo[0]]
+    pos_2 = board.grid[win_combo[1]]
+    pos_3 = board.grid[win_combo[2]]
+
+    if [pos_1, pos_2, pos_3].uniq.count == 1 
+       print "Player #{pos_1} wins!"
+    end
+   } 
 end
